bff7d85e966aeb4aeab9dd06642c3e0d5d98b76d
Artem <amtsik@email.ua>
2020-06-03 14:22:54 +0300

    remove datatables-demo.js

--- admin/templates/script_js.php
+++ admin/templates/script_js.php
@@ -16,4 +16,4 @@
 <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
 <!-- Page level custom scripts -->
-<script src="js/demo/datatables-demo.js"></script>
+<!--<script src="js/demo/datatables-demo.js"></script>-->
